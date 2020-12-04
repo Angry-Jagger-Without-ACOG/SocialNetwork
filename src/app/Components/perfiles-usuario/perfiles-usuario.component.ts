97ffd0618a59b4fffc701c26fa91282e634af32b
Angry-Jagger-Without-ACOG <dani.fotograf@gmail.com>
2020-12-04 20:37:03 +0100

    retoques

--- src/app/Components/perfiles-usuario/perfiles-usuario.component.ts
+++ src/app/Components/perfiles-usuario/perfiles-usuario.component.ts
@@ -14,14 +14,14 @@ export class PerfilesUsuarioComponent implements OnInit {
 
   Llenar_Equipo() {
     this.Perfiles.push(new Perfil('Marc', 'Valdivia', 'brazosrotos@gmail.com', 'roto', 'assets/Imagenes/valdivia.png', 20, 'Un tio con los brazos rotos'));
-    this.Perfiles.push(new Perfil('Ichigo', 'Kurosaki', 'bleach@gmail.com', 'Zangetsu', 'assets/Imagenes/ichigo.jpg', 18, 'Shinigami Sustituto'));
-    this.Perfiles.push(new Perfil('Byakuya', 'Kuchiki', 'bleach@gmail.com', 'Senbonzakura', 'assets/Imagenes/valdivia.png', 200, 'Capitan Shinigami esquadorn 6'));
-    this.Perfiles.push(new Perfil('Renji', 'Abarai', 'bleach@gmail.com', 'Zubimaru', 'assets/Imagenes/valdivia.png', 150, 'Teniente Shinigami esquadorn 6'));
-    this.Perfiles.push(new Perfil('Toshiro', 'Hitsugaya', 'bleach@gmail.com', 'Hyorinmaru', 'assets/Imagenes/valdivia.png', 100, 'Capitan Shinigami esquadorn 10'));
-    this.Perfiles.push(new Perfil('Rukia', 'Kuchiki', 'bleach@gmail.com', 'Sode_No', 'assets/Imagenes/valdivia.png', 150, 'Shinigami'));
-    this.Perfiles.push(new Perfil('Kisuke', 'Urahara', 'bleach@gmail.com', 'Benihime', 'assets/Imagenes/valdivia.png', 300, 'Shinigami'));
-    this.Perfiles.push(new Perfil('Genryusai', 'Yamamoto', 'bleach@gmail.com', 'Ryuijin', 'assets/Imagenes/valdivia.png', 1000, 'Capitan Shinigami esquadron 1'));
-    this.Perfiles.push(new Perfil('Chad', 'Sado', 'bleach@gmail.com', 'BrazodelDiablo', 'assets/Imagenes/valdivia.png', 17, 'Humano con habilidades especiales'));
+    this.Perfiles.push(new Perfil('Ichigo', 'Kurosaki', 'bleach@gmail.com', 'Zangetsu', 'assets/Imagenes/Ichigo.jpg', 18, 'Shinigami Sustituto'));
+    this.Perfiles.push(new Perfil('Byakuya', 'Kuchiki', 'bleach@gmail.com', 'Senbonzakura', 'assets/Imagenes/Byakuya.png', 200, 'Capitan Shinigami esquadorn 6'));
+    this.Perfiles.push(new Perfil('Renji', 'Abarai', 'bleach@gmail.com', 'Zubimaru', 'assets/Imagenes/Renji.png', 150, 'Teniente Shinigami esquadorn 6'));
+    this.Perfiles.push(new Perfil('Toshiro', 'Hitsugaya', 'bleach@gmail.com', 'Hyorinmaru', 'assets/Imagenes/Toshiro.png', 100, 'Capitan Shinigami esquadorn 10'));
+    this.Perfiles.push(new Perfil('Rukia', 'Kuchiki', 'bleach@gmail.com', 'Sode_No', 'assets/Imagenes/Rukia.png', 150, 'Shinigami'));
+    this.Perfiles.push(new Perfil('Kisuke', 'Urahara', 'bleach@gmail.com', 'Benihime', 'assets/Imagenes/Kisuke.png', 300, 'Shinigami'));
+    this.Perfiles.push(new Perfil('Genryusai', 'Yamamoto', 'bleach@gmail.com', 'Ryuijin', 'assets/Imagenes/Viejo.jpg', 1000, 'Capitan Shinigami esquadron 1'));
+    this.Perfiles.push(new Perfil('Yasutora', 'Sado', 'bleach@gmail.com', 'BrazodelDiablo', 'assets/Imagenes/Sado.jpg', 17, 'Humano con habilidades especiales'));
   }
 
   constructor() { }
